b955a93fbcb5d939ef8c7bffe55744909ba936a4
TarakeswariTalla <154327119+TallaTarakeswari@users.noreply.github.com>
2026-02-18 11:26:07 +0530

    Create numpyBasics.py

---
+++ Linear_algebra/numpyBasics.py
@@ -0,0 +1,12 @@
+import numpy as np
+li=[1,2,3]
+arr_1d = np.array([1,2,3])  #1D array
+arr_row = np.array([[1,2,3]]) #row
+arr_col = np.array([[1],[2],[3]]) # columns
+print(li)
+print(arr_row)
+print(arr_col)
+print(arr_1d.shape)
+print(arr_row.shape)
+print(arr_col.shape)
+
